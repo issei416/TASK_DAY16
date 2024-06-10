d0a720e0c8267dcb1a41392bbd381b75a08afbad
issei416 <mukeshkj2912@gmail.com>
2024-06-10 20:23:55 +0530

    callback hell

--- script/script.js
+++ script/script.js
@@ -3,38 +3,27 @@ let text = document.querySelector("#text");
 
 let count = 10;
 let x = () => {
-    timer.textContent = count;
-    timer.classList.add('pop');
+    timer.textContent = count
     setTimeout(() => {
         timer.textContent = count--;
-        timer.classList.add('pop');
         setTimeout(()=>{
             timer.textContent=count--;
-            timer.classList.add('pop');
             setTimeout(()=>{
                 timer.textContent=count--;
-                timer.classList.add('pop');
                 setTimeout(()=>{
-                    timer.textContent=count--;
-                    timer.classList.add('pop');
+                    timer.textContent=count--;        
                     setTimeout(()=>{
-                        timer.textContent=count--;
-                        timer.classList.add('pop');
+                        timer.textContent=count--;          
                         setTimeout(()=>{
-                            timer.textContent=count--;
-                            timer.classList.add('pop');
+                            timer.textContent=count--;               
                             setTimeout(()=>{
-                                timer.textContent=count--;
-                                timer.classList.add('pop');
+                                timer.textContent=count--;                  
                                 setTimeout(()=>{
-                                    timer.textContent=count--;
-                                    timer.classList.add('pop');
+                                    timer.textContent=count--;                   
                                     setTimeout(()=>{
-                                        timer.textContent=count--;
-                                        timer.classList.add('pop');
+                                      timer.textContent=count--;                           
                                         setTimeout(()=>{
-                                            timer.textContent=count--;
-                                            timer.classList.add('pop');
+                                            timer.textContent=count--;                                
                                             setTimeout(()=>{
                                                 timer.style.display="none";
                                                 text.style.display="block";
